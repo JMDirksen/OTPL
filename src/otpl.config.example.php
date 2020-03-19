35726c39111dbe9d13da32d1454d4f79e5ce2351
Jefta Dirksen <jeftadirksen@gmail.com>
2020-03-19 11:02:00 +0100

    Fix: config not returning defaults
    getenv returns FALSE, Null coalescing treats FALSE as a valid value

--- src/otpl.config.example.php
+++ src/otpl.config.example.php
@@ -1,7 +1,7 @@
 <?php
-$email      = getenv('EMAIL')       ?? 'admin@domain.com';
-$expireDays = getenv('EXPIRE_DAYS') ?? 7;
-$title      = getenv('PAGE_TITLE')  ?? 'One Time Password Link';
-$logo       = getenv('LOGO')        ?? 'logo.png';
-$cssFile    = getenv('CSS')         ?? 'otpl.css';
-$jsonFile   = getenv('JSON')        ?? 'otpl.json';
+$email      = getenv('EMAIL')       ? getenv('EMAIL')       : 'admin@domain.com';
+$expireDays = getenv('EXPIRE_DAYS') ? getenv('EXPIRE_DAYS') : 7;
+$title      = getenv('PAGE_TITLE')  ? getenv('PAGE_TITLE')  : 'One Time Password Link';
+$logo       = getenv('LOGO')        ? getenv('LOGO')        : 'logo.png';
+$cssFile    = getenv('CSS')         ? getenv('CSS')         : 'otpl.css';
+$jsonFile   = getenv('JSON')        ? getenv('JSON')        : 'otpl.json';
